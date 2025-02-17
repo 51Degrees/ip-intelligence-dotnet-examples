0e4c075672f5780e697debf4577134548ca610d1
Ben51Degrees <ben@51degrees.com>
2025-02-17 15:42:50 +0000

    FEAT: Added pipeline to automatically generate data classes.

---
+++ ci/generate-accessors.ps1
@@ -0,0 +1,8 @@
+
+./tools/ci/generate-dd-accessors.ps1
+
+$ToolsPath = [IO.Path]::Combine($pwd, "tools")
+$DdPath = [IO.Path]::Combine($pwd, "ip-intelligence-dotnet")
+
+Copy-Item "$ToolsPath/CSharp/IIpIntelligenceData.cs" "$DdPath/FiftyOne.IpIntelligence.Data/Data/"
+Copy-Item "$ToolsPath/CSharp/IpIntelligenceDataBase.cs" "$DdPath/FiftyOne.IpIntelligence.Data/"
