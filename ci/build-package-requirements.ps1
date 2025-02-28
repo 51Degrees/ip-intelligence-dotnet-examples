8ac7824d6675954bcc3a42a28e877c23d7e56c46
Maksym Kucherov <maximus.the0@gmail.com>
2025-02-28 10:30:47 +0200

    CI: Fix script file names. pt.2

--- ci/build-package-requirements.ps1
+++ ci/build-package-requirements.ps1
@@ -1,5 +1,5 @@
 param(
-    [string]$ProjectDir = "FiftyOne.IpIntelligence/FiftyOne.IpIntelligence.Engine.OnPremise.Native",
+    [string]$ProjectDir = "FiftyOne.IpIntelligence/FiftyOne.IpIntelligence.Engine.OnPremise",
     [string]$Name = "Release_x64",
     [Parameter(Mandatory=$true)]
     [string]$RepoName,
