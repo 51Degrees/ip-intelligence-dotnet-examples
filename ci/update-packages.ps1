5e9ab14f9fc114d8ad615bade1b0d6111dc21fa1
Maksym Kucherov <maximus.the0@gmail.com>
2025-04-04 18:00:51 +0300

    CI: Add github as package source for updates.

--- ci/update-packages.ps1
+++ ci/update-packages.ps1
@@ -1,10 +1,17 @@
-
 param(
+    [Parameter(Mandatory=$true)]
+    [string]$OrgName,
+    [string]$GitHubUser = "Automation51D",
     [string]$ProjectDir = ".",
     [string]$Name,
     [Parameter(Mandatory=$true)]
     [string]$RepoName
 )
+
+./dotnet/add-nuget-source.ps1 `
+    -Source "https://nuget.pkg.github.com/$OrgName/index.json" `
+    -UserName $GitHubUser `
+    -Key $env:GITHUB_TOKEN
 
 ./dotnet/run-update-dependencies.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name
 
