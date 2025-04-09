30193aaf344f3f2860f5f09d4d430d4f0c64bda1
Maksym Kucherov <maximus.the0@gmail.com>
2025-04-09 14:54:08 +0300

    CI: Update `build-project.ps1`

--- ci/build-project.ps1
+++ ci/build-project.ps1
@@ -11,16 +11,13 @@ $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 
 if ($BuildMethod -eq "dotnet"){
-
-    if ($IsWindows) {
-        # Ignore Framwork-Web errors for now
-        ./dotnet/build-project-core.ps1 -RepoName $RepoName -ProjectDir "$ProjectDir/FiftyOne.IpIntelligence.Examples.Core.slnf" -Name $Name -Configuration $Configuration -Arch $Arch
-    }
-    else {
-        # On non-Windows, use the solution filter to remove the Framework projects.
-        ./dotnet/build-project-core.ps1 -RepoName $RepoName -ProjectDir "$ProjectDir/FiftyOne.IpIntelligence.Examples.Core.slnf" -Name $Name -Configuration $Configuration -Arch $Arch
-    }
-
+    # Use the solution filter to remove the Framework projects.
+    ./dotnet/build-project-core.ps1 `
+        -RepoName $RepoName `
+        -ProjectDir "$ProjectDir/FiftyOne.IpIntelligence.Examples.Core.slnf" `
+        -Name $Name `
+        -Configuration $Configuration `
+        -Arch $Arch
 }
 else{
     ./dotnet/build-project-framework.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -Configuration $Configuration -Arch $Arch
